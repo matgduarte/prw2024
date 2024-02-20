2b17eb8d28bb44e603d60eba6c0e5e37c9ccf79a
matgduarte <matheusginezduarte@gmail.com>
2024-02-20 11:48:54 -0300

    a

---
+++ php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <header style="background: red; height: 45px;">
+<?php
+    echo "ola ifsp murilo";
+    echo "<div style='background: blue; height: 45px;'>aaa</div>";
+    echo "ola ifsp murilo";
+?>
+</header>
+<main>
+
+</main>
+</body>
+</html>
